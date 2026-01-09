df9e36910cc38357949d77818c7dfe2c57df3224
6633802445_oonchira <oonchira.s@kkumail.com>
2026-01-10 00:47:09 +0700

    test

--- apps/lab04-photo-vue/src/composables/usePhotoGallery.ts
+++ apps/lab04-photo-vue/src/composables/usePhotoGallery.ts
@@ -1,6 +1,9 @@
-import { ref } from 'vue';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
-
+import { ref, onMounted, watch } from 'vue';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+import { isPlatform } from '@ionic/vue';
+import { Capacitor } from '@capacitor/core';
 
 export interface UserPhoto {
   filepath: string;
@@ -8,28 +11,101 @@ export interface UserPhoto {
 }
 
 export const usePhotoGallery = () => {
-  
   const photos = ref<UserPhoto[]>([]);
+  const PHOTO_STORAGE = 'photos';
+
+  
+  const loadSaved = async () => {
+    const photoList = await Preferences.get({ key: PHOTO_STORAGE });
+    const photosInPreferences = photoList.value ? JSON.parse(photoList.value) : [];
+
+    
+    if (!isPlatform('hybrid')) {
+      for (const photo of photosInPreferences) {
+        const file = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data,
+        });
+        
+        photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+      }
+    }
+    photos.value = photosInPreferences;
+  };
+
+  
+  const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+    let base64Data: string | Blob;
+
+    
+    if (isPlatform('hybrid')) {
+      const file = await Filesystem.readFile({
+        path: photo.path!,
+      });
+      base64Data = file.data;
+    } else {
+      
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+      base64Data = (await convertBlobToBase64(blob)) as string;
+    }
+
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    if (isPlatform('hybrid')) {
+      
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    } else {
+      
+      return {
+        filepath: fileName,
+        webviewPath: photo.webPath,
+      };
+    }
+  };
+
+  
+  const convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
 
   
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
-      resultType: CameraResultType.Uri, 
-      source: CameraSource.Camera,      
-      quality: 100,                     
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
     });
 
     const fileName = new Date().getTime() + '.jpeg';
+    const savedFileImage = await savePicture(photo, fileName);
 
-   
-    photos.value = [
-      {
-        filepath: fileName,
-        webviewPath: photo.webPath 
-      },
-      ...photos.value,
-    ];
+    photos.value = [savedFileImage, ...photos.value];
   };
+
+  
+  watch(photos, (newPhotos) => {
+    Preferences.set({
+      key: PHOTO_STORAGE,
+      value: JSON.stringify(newPhotos),
+    });
+  });
+
+  
+  onMounted(loadSaved);
 
   return {
     photos,
